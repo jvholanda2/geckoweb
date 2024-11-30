dc2da5ed46eeea9e0d92f9738dd41a89ad7668d7
jvholanda2 <jvholanda18@gmail.com>
2024-11-30 03:26:06 -0300

    Finaliza a estilizaçao da view whoWillPlay, com o detalhe de descobrir como adicionar a o X da listagem de jogadores, depois de adicionado no JS.

--- frontend/static/js/views/WhoWillPlay.js
+++ frontend/static/js/views/WhoWillPlay.js
@@ -12,6 +12,43 @@ export default class from extends AbstractView {
 
     async getHtml() {
         return ` 
+            <div class="gecko_view">
+                <div class="gecko_div_title">
+                    <h1 id="gecko_div_title_title" class="gecko_title">GeckoWeb MVP</h1>
+                </div>
+                <div class="gecko_wrap">
+                    <h2 class="gecko_title">Quem vai jogar?</h2>
+                    <p class="gecko_text">Coloque aqui o nome do jogador que quer adicionar &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+                    <input class="gecko_input" type="text" placeholder="">
+                    <button class="gecko_button_main">Adicionar</button>
+
+                    <h2 class="gecko_title">Participantes</h2>
+                    <p class="gecko_text">Aqui estão todos os participantes até o momento</p>
+                    <div id="gecko_players_list">
+                        <div class="gecko_players_list_item">
+                            <p>jogador1</p>
+                            <img src="static/assets/x.png" alt="">
+                        </div>
+                        <div class="gecko_players_list_item">
+                            <p>jogador1</p>
+                            <img src="static/assets/x.png" alt="">
+                        </div>
+                        <div class="gecko_players_list_item">
+                            <p>jogador1</p>
+                            <img src="static/assets/x.png" alt="">
+                        </div>
+                        <div class="gecko_players_list_item">
+                            <p>jogador1</p>
+                            <img src="static/assets/x.png" alt="">
+                        </div>
+                        <div class="gecko_players_list_item">
+                            <p>jogador1</p>
+                            <img src="static/assets/x.png" alt="">
+                        </div>
+                    </div>
+                    <a class="gecko_button_main" href="Themes" id="gecko_button_bottom">Jogar</a>
+                </div>
+            </div>
             <a href="/dashboard" data-lin>Go To dashboard</a>
         `; 
     }
