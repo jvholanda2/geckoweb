cc84aa070586dfbf839616d7643dae0b60b4bc00
João Vitor <jvholanda18@gmail.com>
2025-08-09 13:22:25 -0300

    Adiciona margem para posicionar botao na parte de baixo da tela

--- frontend/static/js/views/StartTheRound.js
+++ frontend/static/js/views/StartTheRound.js
@@ -47,7 +47,7 @@ export default class extends AbstractView {
                 <div class="gecko_wrap_start">
                     <img src="static/assets/profile-question.png" alt="">
                     <span class="gecko_title">Que os jogos comecem!</span>
-                    <p class="gecko_text gecko_text_bold">Hora de descobrir quem é o impostor!</p>
+                    <p class="gecko_text gecko_text_bold gecko_margin_bottom_end">Hora de descobrir quem é o impostor!</p>
                 </div>
                 <a class="gecko_button_main" href="Themes" class="gecko_button_bottom">Nova Rodada!</a>
                 <a class="gecko_button_secondary" href="WhoWillPlay" class="gecko_button_bottom">Voltar ao Início!</a>
